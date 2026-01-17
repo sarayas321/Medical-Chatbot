e7dbb27fbc1c4fe9ca16b4b9ba47cf45171c25fc
Sara Samkari <sarah.yousef91@hotmail.com>
2026-01-17 12:17:17 +0300

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+# creating directory
+mkdir -p src
+mkdir -p research
+
+# creatung files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully!."
